8765c06b8e1b6a91bb6943e33433e2c2e3afba81
이로움 <roum02@jobkorea.co.kr>
2022-06-07 23:39:12 +0900

    useForm 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import useForm from 'react-hook-form';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-function App() {
+export default function App() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
   return <div className='App'>test</div>;
 }
-
-export default App;
